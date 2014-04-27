7455dad39d2e6b37692b72bbc59d36c16cce51f2
Talon Daniels <talondaniels@gmail.com>
2014-04-27 02:01:38 -0600

    more error handling & implemented imgur uploading

--- server.go
+++ server.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"math/rand"
+	"mime/multipart"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/go-martini/martini"
@@ -72,33 +78,107 @@ func receiveImage(res http.ResponseWriter, req *http.Request, params martini.Par
 		fmt.Println("Found key '" + imageKey + "'")
 	} else {
 		fmt.Println("Could not find required key")
-		res.Write([]byte("Could not find required X-Mapper-Key header"))
+		http.Error(res, "Could not find required X-Mapper-Key header", http.StatusBadRequest)
 		return
 	}
 
 	file, _, err := req.FormFile("file")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Could not find 'file' parameter.")
+		http.Error(res, "Could not find 'file' parameter", http.StatusBadRequest)
+		return
 	}
+
+	fmt.Println("Size: " + req.Header.Get(""))
 
 	data, err := ioutil.ReadAll(file)
 	if err != nil {
 		fmt.Println(err)
+		http.Error(res, "internal error", http.StatusInternalServerError)
+		return
 	}
 
 	err = ioutil.WriteFile(params["id"]+".jpg", data, 0664)
 	if err != nil {
 		fmt.Println(err)
+		http.Error(res, "internal error", http.StatusInternalServerError)
+		return
 	}
 	file.Close()
 
 	fmt.Println("Saved " + params["id"] + ".jpg")
-	sendToImgur(params["id"])
-	http.Redirect(res, req, "/img/"+params["id"], http.StatusTemporaryRedirect)
+	imgurResponse, err := sendToImgur(params["id"])
+	if err != nil {
+		fmt.Println("Failed to upload to imgur")
+		http.Redirect(res, req, "/img/"+params["id"], http.StatusTemporaryRedirect)
+	}
+	fmt.Println(params["id"] + " -> " + imgurResponse.Data.Link)
+	//http.Redirect(res, req, imgurResponse.Data.Link, http.StatusTemporaryRedirect)
+	res.Write([]byte(imgurResponse.Data.Link))
+
 }
 
-func sendToImgur(imageId string) {
+func sendToImgur(imageId string) (*ImgurResponse, error) {
 
+	var imageBytes bytes.Buffer
+	imageWriter := multipart.NewWriter(&imageBytes)
+
+	imageFile, err := os.Open(imageId + ".jpg")
+
+	if err != nil {
+		fmt.Println("Couldn't open file " + imageId + ".jpg")
+		return nil, errors.New("Couldn't open file " + imageId + ".jpg")
+	}
+
+	formWriter, err := imageWriter.CreateFormFile("image", imageId+".jpg")
+	if err != nil {
+		fmt.Println("Couldn't create file form")
+		return nil, errors.New("Couldn't create file form")
+	}
+
+	if _, err = io.Copy(formWriter, imageFile); err != nil {
+		fmt.Println("Couldn't copy image data in to form")
+		return nil, errors.New("Couldn't copy image data in to form")
+	}
+
+	imageWriter.Close()
+
+	req, err := http.NewRequest("POST", IMGUR_API_ENDPOINT, &imageBytes)
+
+	if err != nil {
+		fmt.Println("Couldn't create http request")
+		return nil, errors.New("Couldn't create http request")
+	}
+
+	req.Header.Set("Content-Type", imageWriter.FormDataContentType())
+	req.Header.Set("Authorization", "Client-ID "+imgurKey)
+
+	fmt.Println("Uploading " + imageId + ".jpg to imgur...")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Did not get a response from imgur")
+		return nil, errors.New("Did not get a response from imgur")
+	}
+
+	if res.StatusCode != http.StatusOK {
+		err = fmt.Errorf("bad status: %s", res.Status)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Could not read body from imgur")
+		return nil, errors.New("Could not read body from imgur")
+	}
+
+	var imgurResponse ImgurResponse
+	err = json.Unmarshal(body, &imgurResponse)
+	if err != nil {
+		fmt.Println("Couldn't decode json response from imgur: " + string(body))
+	}
+	fmt.Println("Done uploading " + imageId)
+	return &imgurResponse, nil
 }
 
 func generateImageId(length int) string {
@@ -112,5 +192,17 @@ func generateImageId(length int) string {
 var UrlCharacters = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
 
 const (
-	ID_LENGTH = 8
+	ID_LENGTH            = 8
+	IMGUR_API_ENDPOINT   = "https://api.imgur.com/3/image"
+	MAX_IMAGE_SIZE_BYTES = 1024 * 5
 )
+
+type ImgurResponse struct {
+	Data *ImgurData
+}
+
+type ImgurData struct {
+	Id         string
+	Deletehash string
+	Link       string
+}
